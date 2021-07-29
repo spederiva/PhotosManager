36ca63dc938c9e23e10c0475e1c6f40c746d36da
Sebastian Pederiva <sebastian.pederiva@opora.io>
2021-07-29 09:51:49 +0300

    config

--- REST/PhotoFrame/config.js
+++ REST/PhotoFrame/config.js
@@ -17,10 +17,11 @@
 const config = {};
 
 // The OAuth client ID from the Google Developers console.
-config.oAuthClientID = 'ADD YOUR CLIENT ID';
+config.oAuthClientID = '';
+
 
 // The OAuth client secret from the Google Developers console.
-config.oAuthclientSecret = 'ADD YOUR CLIENT SECRET';
+config.oAuthclientSecret = '';
 
 // The callback to use for OAuth requests. This is the URL where the app is
 // running. For testing and running it locally, use 127.0.0.1.
@@ -32,8 +33,9 @@ config.port = 8080;
 // The scopes to request. The app requires the photoslibrary.readonly and
 // plus.me scopes.
 config.scopes = [
-  'https://www.googleapis.com/auth/photoslibrary.readonly',
-  'profile',
+    'https://www.googleapis.com/auth/photoslibrary.readonly',
+    'https://www.googleapis.com/auth/photoslibrary.appendonly',
+    'profile',
 ];
 
 // The number of photos to load for search requests.
